45ba63f701758f4b8339f2fa22161bf8f459dbf4
dihasananda <dihasananda@gmail.com>
2024-05-17 08:45:29 +0700

    overloadable operator

--- src/main.rs
+++ src/main.rs
@@ -1099,3 +1099,28 @@ impl<T>  GetValue<T> for Point<T> where T: PartialOrd {
         &self.x
     }
 }
+
+use core::ops::Add;
+
+struct Apple {
+   quantity: i32,
+}
+
+impl Add for Apple {
+    type Output = Apple;
+
+    fn add(self, rhs: Self) -> Self::Output {
+      Apple {
+         quantity: self.quantity + rhs.quantity
+      }
+    }
+}
+
+#[test]
+fn test_operator_add() {
+    let apple1 = Apple{quantity: 10};
+    let apple2 = Apple{quantity: 20};
+
+    let apple3 = apple1 + apple2;
+    println!("{}", apple3.quantity);
+}
