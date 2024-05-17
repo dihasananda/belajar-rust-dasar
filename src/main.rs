092e36a821a8e3eec9d42d9f412c46f42cd6ec11
dihasananda <dihasananda@gmail.com>
2024-05-17 14:26:45 +0700

    optional values

--- src/main.rs
+++ src/main.rs
@@ -1125,3 +1125,19 @@ fn test_operator_add() {
     let apple3 = apple1 + apple2;
     println!("{}", apple3.quantity);
 }
+
+fn double(value: Option<i32>) -> Option<i32> {
+   match value {
+      None => None,
+      Some(i) => Some(i *2),
+   }
+}
+
+#[test]
+fn test_option() {
+   let result = double(Some(10));
+   println!("{:?}", result);
+
+   let result = double(None);
+   println!("{:?}", result);
+}
