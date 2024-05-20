a2ed95277d6f271c7f312f6d8ba81f7841363b08
dihasananda <dihasananda@gmail.com>
2024-05-20 22:07:33 +0700

    lifetime

--- src/main.rs
+++ src/main.rs
@@ -1546,3 +1546,104 @@ fn test_connect_app() {
         }
     }
 }
+
+//lifetime
+
+#[test]
+fn test_dangling_reference() {
+    let r: &i32;
+    {
+      let _x = 5;
+      // r = &x; error
+    }
+
+    r = &40;
+    println!("r: {}", r);
+}
+
+// lifetime annotation syntax
+fn longest<'a>(value1: &'a str, value2: &'a str) -> &'a str {
+   if value1.len() > value2.len() {
+      value1
+   } else {
+      value2
+   }
+}
+
+#[test]
+fn test_lifetime_annotation() {
+    let value1 = "dihas";
+    let value2 = "ananda";
+    let result = longest(value1, value2);
+    println!("result: {}", result);
+}
+
+#[test]
+fn test_lifetime_annotation_dangling_reference() {
+    let string1 = String::from("ananda");
+    let string2 = String::from("dihas");
+    let result;
+    {
+      result = longest(string1.as_str(), string2.as_str())
+    }
+    println!("result: {}", result);
+}
+
+// lifetime annotation di struct
+
+struct Student<'a, 'b> {
+   name: &'a str,
+   last_name: &'b str,
+}
+
+impl<'a, 'b> Student<'a, 'b> {
+    fn longest_name(&self, student: &Student<'a, 'b>) -> &'a str {
+      if self.name.len() > student.name.len() {
+         self.name
+      } else {
+          student.name
+      }
+    }
+}
+
+fn longest_student_name<'a, 'b>(student1: &Student<'a, 'b>, student2: &Student<'a, 'b>) -> &'a str {
+   if student1.name.len() > student2.name.len() {
+       student1.name
+   } else {
+      student2.name
+   }
+}
+
+#[test]
+fn test_student() {
+    let student = Student{
+      name: "dihas",
+      last_name: "ananda",
+    };
+    println!("{} {}", student.name, student.last_name);
+
+    let student2 = Student {
+      name: "ananda",
+      last_name: "dihas"
+    };
+    let result = longest_student_name(&student, &student2);
+    println!("{}", result);
+
+    let result = student.longest_name(&student2);
+    println!("{}", result);
+    
+}
+
+struct Teacher<'a, ID> where ID: Ord {
+   id: ID,
+   name:  &'a str,
+}
+
+#[test]
+fn test_lifetime_annotation_generic() {
+    let teacher = Teacher {
+      id: 10,
+      name: "dihas",
+    };
+    println!("{} {}", teacher.id, teacher.name)
+}
